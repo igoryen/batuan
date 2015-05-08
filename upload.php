93974647ea73fc6eeb3257de7a67df42c9fadc82
igoryen <igor.e@advertiseit.com>
2015-05-08 09:36:01 -0400

    upload.php: new code

--- upload.php
+++ upload.php
@@ -1,8 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$target_dir    = "uploads/";
+$target_file   = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk      = 1;
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if ($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  }
+  else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
