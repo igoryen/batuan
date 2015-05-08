2c863f8cc04195376795db917cdf2dea7bdf423e
igoryen <igor.e@advertiseit.com>
2015-05-08 09:32:50 -0400

    New html form

--- index.php
+++ index.php
@@ -20,5 +20,10 @@ and open the template in the editor.
   </head>
   <body>
     <div><h1>Batuan</h1></div>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+      Select image to upload:
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit" value="Upload Image" name="submit">
+    </form>
   </body>
 </html>
